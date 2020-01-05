0e3d9930d8047c7e2261acf06aaa41e6f7c0858d
HUANG XUAN KUN <hxk123123123@gmail.com>
2020-01-06 02:35:35 +0800

    Initial Commit

---
+++ src/main/java/com/xk/freddit/FredditApplication.java
@@ -0,0 +1,13 @@
+package com.xk.freddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FredditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FredditApplication.class, args);
+    }
+
+}
