4f5c07289ea0da4204820b28770434f2ad274e75
HUANG XUAN KUN <hxk123123123@gmail.com>
2020-01-06 03:55:04 +0800

    add logger

--- src/main/java/com/xk/freddit/FredditApplication.java
+++ src/main/java/com/xk/freddit/FredditApplication.java
@@ -1,13 +1,19 @@
 package com.xk.freddit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class FredditApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(FredditApplication.class, args);
     }
-
 }
