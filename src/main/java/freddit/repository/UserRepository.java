2a843583447d1932b0158c8285779a86b156d2b1
HUANG XUAN KUN <hxk123123123@gmail.com>
2020-01-07 17:45:38 +0800

    added authentication

--- src/main/java/freddit/repository/UserRepository.java
+++ src/main/java/freddit/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package freddit.repository;
 
-public interface UserRepository {
+import freddit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/freddit/security/SecurityConfiguration.java
+++ src/main/java/freddit/security/SecurityConfiguration.java
@@ -1,4 +1,50 @@
 package freddit.security;
 
-public class SecurityConfiguration {
+import freddit.service.UserDetailsServiceImpl;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                .antMatchers("/actuator/").hasRole("ACTUATOR")
+                .antMatchers("/link/submit").hasRole("USER")
+                .antMatchers("/link/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+            .and()
+                .formLogin()
+                    .loginPage("/login").permitAll()
+                .usernameParameter("email") //to get the email field from the form
+            .and()
+                .logout()
+            .and()
+                .rememberMe()
+            .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
 }
