e19b62d9e658008208fbeeb256845c0d3568f5c9
HUANG XUAN KUN <hxk123123123@gmail.com>
2020-01-09 02:14:40 +0800

    updated base info to db

--- src/main/java/freddit/runner/DatabaseLoader.java
+++ src/main/java/freddit/runner/DatabaseLoader.java
@@ -42,18 +42,13 @@ public class DatabaseLoader implements CommandLineRunner {
 
     private void addLinks(){
         Map<String,String> links = new HashMap<>();
-        links.put("Securing Spring Boot APIs and SPAs with OAuth 2.0","https://auth0.com/blog/securing-spring-boot-apis-and-spas-with-oauth2/?utm_source=reddit&utm_medium=sc&utm_campaign=springboot_spa_securing");
-        links.put("Easy way to detect Device in Java Web Application using Spring Mobile - Source code to download from GitHub","https://www.opencodez.com/java/device-detection-using-spring-mobile.htm");
-        links.put("Tutorial series about building microservices with SpringBoot (with Netflix OSS)","https://medium.com/@marcus.eisele/implementing-a-microservice-architecture-with-spring-boot-intro-cdb6ad16806c");
-        links.put("Detailed steps to send encrypted email using Java / Spring Boot - Source code to download from GitHub","https://www.opencodez.com/java/send-encrypted-email-using-java.htm");
-        links.put("Build a Secure Progressive Web App With Spring Boot and React","https://dzone.com/articles/build-a-secure-progressive-web-app-with-spring-boo");
-        links.put("Building Your First Spring Boot Web Application - DZone Java","https://dzone.com/articles/building-your-first-spring-boot-web-application-ex");
-        links.put("Building Microservices with Spring Boot Fat (Uber) Jar","https://jelastic.com/blog/building-microservices-with-spring-boot-fat-uber-jar/");
-        links.put("Spring Cloud GCP 1.0 Released","https://cloud.google.com/blog/products/gcp/calling-java-developers-spring-cloud-gcp-1-0-is-now-generally-available");
-        links.put("Simplest way to Upload and Download Files in Java with Spring Boot - Code to download from Github","https://www.opencodez.com/uncategorized/file-upload-and-download-in-java-spring-boot.htm");
-        links.put("Add Social Login to Your Spring Boot 2.0 app","https://developer.okta.com/blog/2018/07/24/social-spring-boot");
-        links.put("File download example using Spring REST Controller","https://www.jeejava.com/file-download-example-using-spring-rest-controller/");
-
+        links.put("How I build a reddit with Spring boot?","https://github.com/HUANGXUANKUN/clone-reddit-spring-boot");
+        links.put("A short demo of my 2113 Project","https://www.youtube.com/watch?v=GdsyMKeBYHg");
+        links.put("How I crawl Leetcode problems using Selenium and Requests","https://github.com/HUANGXUANKUN/leetcode-summary-generator");
+        links.put("How I crawl top rated movie using Scrapy?","https://github.com/HUANGXUANKUN/douban-movie-crawler");
+        links.put("Building a maze game with Xuan Kun","https://youtu.be/OkyE1ssDXnQ");
+        links.put("How I build a maplestory-like game with Pygame","https://www.youtube.com/watch?v=GdsyMKeBYHg");
+        links.put("Building a simple shooting game with Pygame","https://github.com/HUANGXUANKUN/more-than-shooting");
         links.forEach((k,v) -> {
             User u1 = users.get("user@gmail.com");
             User u2 = users.get("super@gmail.com");
@@ -67,9 +62,9 @@ public class DatabaseLoader implements CommandLineRunner {
             linkRepository.save(link);
 
             // we will do something with comments later
-            Comment spring = new Comment("Thank you for this link related to Spring Boot. I love it, great post!",link);
-            Comment security = new Comment("I love that you're talking about Spring Security",link);
-            Comment pwa = new Comment("What is this Progressive Web App thing all about? PWAs sound really cool.",link);
+            Comment spring = new Comment("Thank you Xuan Kun. I love it, great post!",link);
+            Comment security = new Comment("OMG Xuan Kun you are awesome",link);
+            Comment pwa = new Comment("Cool",link);
             Comment comments[] = {spring,security,pwa};
             for(Comment comment : comments) {
                 commentRepository.save(comment);
@@ -90,20 +85,20 @@ public class DatabaseLoader implements CommandLineRunner {
         Role adminRole = new Role("ROLE_ADMIN");
         roleRepository.save(adminRole);
 
-        User user = new User("user@gmail.com",secret,true,"Joe","User","joedirt");
+        User user = new User("user@gmail.com",secret,true,"nobody","User","xuan kun");
         user.addRole(userRole);
         user.setConfirmPassword(secret);
         userRepository.save(user);
         users.put("user@gmail.com",user);
 
-        User admin = new User("admin@gmail.com",secret,true,"Joe","Admin","masteradmin");
+        User admin = new User("admin@gmail.com",secret,true,"nobody","Admin","xuan kun");
         admin.setAlias("joeadmin");
         admin.addRole(adminRole);
         admin.setConfirmPassword(secret);
         userRepository.save(admin);
         users.put("admin@gmail.com",admin);
 
-        User master = new User("super@gmail.com",secret,true,"Super","User","superduper");
+        User master = new User("super@gmail.com",secret,true,"nobody","Super","xuan kun");
         master.addRoles(new HashSet<>(Arrays.asList(userRole,adminRole)));
         master.setConfirmPassword(secret);
         userRepository.save(master);
